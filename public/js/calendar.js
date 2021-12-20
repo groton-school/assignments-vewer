ed84d88401568f71615e1514d78a06306e8d4d52
Seth Battis <sbattis@groton.org>
2021-12-20 08:15:06 -0500

    Styling

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,39 +1,53 @@
 function calendar(data) {
     const content = document.getElementById('content');
     content.innerHTML = '';
-    for (const [year, ydata] of Object.entries(data)) {
-        for (const [month, mdata] of Object.entries(ydata)) {
-            const days = new Date(year, month+1, 0).getDate();
-            console.log(year, month, new Date(year, month, 0));
-            console.log(year, month, new Date(year, month, 1));
+    for (let [year, ydata] of Object.entries(data)) {
+        year = parseInt(year);
+        for (let [month, mdata] of Object.entries(ydata)) {
+            const m = parseInt(month) - 1;
+            const days = new Date(year, m + 1, 0).getDate();
             const calendar = document.createElement('table');
+            calendar.classList.add('calendar')
             content.appendChild(calendar);
-            calendar.innerHTML = `
-                <thead>
-                    <tr>
-                        <th colspan="7">${new Date(year, month, 1).toLocaleString('default', {month: 'long'})}</th>
-                    </tr>
-                </thead>`;
+            const heading = document.createElement('thead');
+            heading.innerHTML = `
+                <tr>
+                    <th colspan="7" class="month">${new Date(year, m, 1).toLocaleString('default', {month: 'long'})}</th>
+                </tr>`;
+                calendar.appendChild(heading);
+            let week = document.createElement('tr');
+            heading.appendChild(week);
+            const padding = new Date(year, m, 1).getDay();
+            for (let weekday = -1 * padding + 1; weekday < (-1 * padding + 1) + 7; weekday++) {
+                const label = document.createElement('th');
+                label.classList.add('weekday');
+                label.innerHTML = new Date(year, m, weekday, padding).toLocaleString('default', {weekday: "long"});
+                week.appendChild(label);
+            }
             grid = document.createElement('tbody');
             calendar.appendChild(grid);
             week = document.createElement('tr');
-            padding = new Date(year, month, 1).getDay();
+            week.classList.add('week');
             if (padding) {
                 for (let pad = 0; pad < padding; pad++) {
-                    week.appendChild(document.createElement('td'));
+                    const day = document.createElement('td');
+                    day.classList.add('day','padding');
+                    week.appendChild(day);
                 }
             }
             for (let day = 1; day <= days; day++) {
-                if (new Date(year, month, day).getDay() === 0) {
+                if (new Date(year, m, day).getDay() === 0) {
                     grid.appendChild(week);
                     week = document.createElement('tr');
                 }
                 cell = document.createElement('td');
-                cell.innerHTML = `<p class="date">${day}</p>`;
+                cell.classList.add('day');
+                cell.innerHTML = `<div class="date">${day}</div>`;
                 list = document.createElement('div');
+                list.classList.add('assignments');
                 cell.appendChild(list);
                 week.appendChild(cell);
-                if (data[year][month][`${day}`]) {
+                if (data[`${year}`][`${month}`][`${day}`]) {
                     for (const assignment of data[year][month][`${day}`]) {
                         entry = document.createElement('div');
                         entry.classList.add('tooltip', 'assignment');
@@ -42,6 +56,7 @@ function calendar(data) {
                             <div class="tooltip text">
                                 <div class="description">${assignment.description}</div>`;
                         list.appendChild(entry);
+                        entry['data-assignment'] = assignment;
                     }
                 }
             }
