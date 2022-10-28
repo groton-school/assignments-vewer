8aff8b57a0b82775784c98f32a434a09ea77cb69
Seth Battis <sbattis@groton.org>
2022-10-28 17:09:10 +0000

    Strip TODOs

--- public/sky-api.php
+++ public/sky-api.php
@@ -6,11 +6,13 @@ use GrotonSchool\AssignmentsViewer\Users\UserFactory;
 use GrotonSchool\OAuth2\Client\Provider\BlackbaudSKY;
 use GuzzleHttp\Client;
 
-require_once __DIR__ . '/../bootstrap.php';
+require_once __DIR__ . "/../bootstrap.php";
 /** @var Container $container */
 
 /** @var User */
-$user = $container->get(UserFactory::class)-> getByInstance($_SESSION[CONSUMER_GUID], $_SESSION[USER_ID]);
+$user = $container
+    ->get(UserFactory::class)
+    ->getByInstance($_SESSION[CONSUMER_GUID], $_SESSION[USER_ID]);
 
 /** @var BlackbaudSKY */
 $sky = $container->get(BlackbaudSKY::class);
@@ -19,12 +21,12 @@ $sky = $container->get(BlackbaudSKY::class);
 $client = $container->get(Client::class);
 
 $request = $sky->getAuthenticatedRequest(
-    $_REQUEST['method'] ?: 'GET',
-    preg_replace('/:user_id/', $user->getUserId(), $_REQUEST['url']), // TODO others?
+    $_REQUEST["method"] ?: "GET",
+    preg_replace("/:user_id/", $user->getUserId(), $_REQUEST["url"]),
     $sky->getAccessToken(),
-    $_REQUEST['options'] ?: []
+    $_REQUEST["options"] ?: []
 );
 $response = $client->send($request);
 
-header('Content-Type: application/json');
+header("Content-Type: application/json");
 echo $response->getBody()->getContents();
